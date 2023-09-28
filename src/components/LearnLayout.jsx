e371557e9acc8d205802bc2b0263351d836e936c
natalia galindo <nataliadgalindo@outlook.com>
2023-09-29 00:30:05 +0100

    add scroll function

--- src/components/LearnLayout.jsx
+++ src/components/LearnLayout.jsx
@@ -1,16 +1,37 @@
 import { NavLink, Outlet } from "react-router-dom"
 
 function LearnLayout() {
+  function scrollToTop() {
+    window.scrollTo(0, 0)
+  }
+
   return (
     <div className="grid-container">
       <aside>
         <nav className="side-nav">
-          <NavLink to="/learn">The Basics</NavLink>
-          <NavLink to="devenv">Development Environment</NavLink>
-          <NavLink to="errors">Common Errors</NavLink>
-          <NavLink to="debugging">Debugging</NavLink>
-          <NavLink to="libraries">Python Libraries</NavLink>
-          <NavLink to="styles">Programming Styles</NavLink>
+          <NavLink to="/learn" onClick={scrollToTop}>
+            The Basics
+          </NavLink>
+
+          <NavLink to="devenv" onClick={scrollToTop}>
+            Development Environment
+          </NavLink>
+
+          <NavLink to="errors" onClick={scrollToTop}>
+            Common Errors
+          </NavLink>
+
+          <NavLink to="debugging" onClick={scrollToTop}>
+            Debugging
+          </NavLink>
+
+          <NavLink to="libraries" onClick={scrollToTop}>
+            Python Libraries
+          </NavLink>
+
+          <NavLink to="styles" onClick={scrollToTop}>
+            Programming Styles
+          </NavLink>
         </nav>
       </aside>
 
