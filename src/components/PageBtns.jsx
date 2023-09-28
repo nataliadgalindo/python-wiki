994ae26e3b4e21da9840c6bddf359ca88e74a476
natalia galindo <nataliadgalindo@outlook.com>
2023-09-28 13:05:14 +0100

    update

--- src/components/PageBtns.jsx
+++ src/components/PageBtns.jsx
@@ -19,11 +19,11 @@ export function NextPageBtn({ name, page }) {
 export function LastPageBtn({ name, page }) {
   return (
     <NavLink to={page} onClick={scrollToTop} className="last-page-btn page-btn">
+      <p>{"<"}</p>
       <div>
         <p>PREVIOUS</p>
         <span>{name}</span>
       </div>
-      <p>{"<"}</p>
     </NavLink>
   )
 }
